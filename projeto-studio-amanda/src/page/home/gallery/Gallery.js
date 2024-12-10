4bd4ff99686b46974e1bf7baaf8b9c197c81a60b
Matheus Felipe <felipe_2012matheus@hotmail.com>
2024-12-10 18:34:59 -0300

    Corrigindo um bug de navegação nos botões do header

--- projeto-studio-amanda/src/page/home/gallery/Gallery.js
+++ projeto-studio-amanda/src/page/home/gallery/Gallery.js
@@ -22,22 +22,22 @@ const Gallery = () => (
                 <p>Conheça um pouco do nosso trabalho</p>
             </div>
             <div className="gallery-panel">
-                <img className="gallery-photo-vertical" src={galleryPhotoVertical1} alt="foto galeria" />
+                <img className="gallery-photo-vertical" src={galleryPhotoVertical1} alt="foto criança dormindo" />
                 <div>
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal1} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal2} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal3} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal4} alt="foto galeria" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal1} alt="foto criança dormindo" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal2} alt="foto criança sorrindo no cenário de São João" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal3} alt="foto criança sorrindo no dcenário do Super Mario" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal4} alt="foto bebê dormindo" />
                 </div>
             </div>
             <div className="gallery-panel">
                 <div>
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal5} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal6} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal7} alt="foto galeria" />
-                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal8} alt="foto galeria" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal5} alt="foto ao ar livre em casal gestante" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal6} alt="foto criança sorrindo dentro de um carrinho" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal7} alt="foto bebê dormindo" />
+                    <img className="gallery-photo-horizontal" src={galleryPhotoHorizontal8} alt="foto criança sorrindo segurando um ursinho" />
                 </div>
-                <img className="gallery-photo-vertical" src={galleryPhotoVertical2} alt="foto galeria" />
+                <img className="gallery-photo-vertical" src={galleryPhotoVertical2} alt="foto adolescente fazendo pose" />
             </div>
             <div className="gallery-footer">
                 <img src={galleryPhoto} alt="rodapé da galeria"/>
--- projeto-studio-amanda/src/page/home/header/Header.js
+++ projeto-studio-amanda/src/page/home/header/Header.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './Header.css';
 import logoHeader from '../../../assets/img/logo.png';
 
+const scrollToGallery = () => {
+    const gallerySection = document.getElementById("gallery");
+    if (gallerySection) {
+        gallerySection.scrollIntoView({ behavior: "smooth" });
+    }
+};
+
+const scrollToContact = () => {
+    const contactSection = document.getElementById("contact");
+    if (contactSection) {
+        contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+};
+
+
 const Header = () => (
     <header className="header">
         <img className="header-img" src={logoHeader} alt="logo site" />
         <nav>
         <ul className="header-nav">
-            <li><a className="nav-option" href="#gallery">Galeria</a></li>
-            <li><a className="nav-option" href="#contact">Contato</a></li>
+            <li><button className="nav-option" onClick={scrollToGallery}>Galeria</button></li>
+            <li><button className="nav-option" onClick={scrollToContact}>Contato</button></li>
         </ul>
         </nav>
     </header>
